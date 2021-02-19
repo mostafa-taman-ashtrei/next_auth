2c3b7ebb49d10cb9618ec1b406063dd534f39d53
NEO <neo.one.mail@gmail.com>
2021-02-19 03:41:52 +0200

    add DB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import Adapters from 'next-auth/adapters';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
     NextAuth(req, res, {
@@ -19,5 +23,6 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
             jwt: true,
             maxAge: 60 * 60 * 24 * 30, // 30 days
         },
+        adapter: Adapters.Prisma.Adapter({ prisma }),
     });
 };
